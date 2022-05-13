ddc59c59d84c6ce270eedfe8aac70d87b498f32b
Julian <perassijulian@gmail.com>
2022-05-13 13:48:16 -0300

    hotel controller

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import { 
+    createHotel,
+    updateHotel,
+    removeHotel,
+    getHotel,
+    getHotels,
+} from "../controllers/hotel.js";
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
-    res.send("Hotels")
-});
+//CREATE
+router.post('/', createHotel)
+//UPDATE
+router.put('/:id', updateHotel)
+//DELETE
+router.delete('/:id', removeHotel)
+//GET
+router.get("/find/:id", getHotel);
+//GETALL
+router.get('/', getHotels)
 
 export default router;
