ea9717794c6b16dc697f1fe37c65ab2922464352
Julian <perassijulian@gmail.com>
2022-05-13 10:09:09 -0300

    add routes

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config()
-const morgan = require('morgan')
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-//database connection
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('DB connected succesfully'))
-    .catch((error) => console.log(error));
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
 
 //middlewares
+app.use(cors())
+app.use(cookieParser())
 app.use(express.json());
-app.use(morgan('dev'));
 
-//routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
-//backend server listener
-app.listen(process.env.PORT || 5000, () => console.log('Server running'))
+app.listen(5000, () => {
+  connect();
+  console.log("Connected to backend.");
+});
